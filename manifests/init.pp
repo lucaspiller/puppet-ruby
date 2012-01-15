9d63d371ee6ca09ef0c1d165e12797adb0dd7448
Luca Spiller <luca@stackednotion.com>
2012-01-15 16:42:33 +0000

    Add path for exec commands (required if a default is not set)

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,15 +15,18 @@ class ruby($version="1.9.3-p0") {
   exec { "alternatives-ruby-$version":
     command => "update-alternatives --install /usr/bin/ruby ruby /opt/ruby-$version/bin/ruby 10 --slave /usr/bin/irb irb /opt/ruby-$version/bin/irb && update-alternatives --set ruby /opt/ruby-$version/bin/ruby",
     unless => "test /etc/alternatives/ruby -ef /opt/ruby-$version/bin/ruby && test /etc/alternatives/irb -ef /opt/ruby-$version/bin/irb",
+    path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"]
   }
   exec { "alternatives-gem-$version":
     command => "update-alternatives --install /usr/bin/gem gem /opt/ruby-$version/bin/gem 10 && update-alternatives --set gem /opt/ruby-$version/bin/gem",
     unless => "test /etc/alternatives/gem -ef /opt/ruby-$version/bin/gem",
+    path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["alternatives-ruby-$version"]
   }
   exec { "alternatives-rake-$version":
     command => "update-alternatives --install /usr/bin/rake rake /opt/ruby-$version/bin/rake 10 && update-alternatives --set rake /opt/ruby-$version/bin/rake",
     unless => "test /etc/alternatives/rake -ef /opt/ruby-$version/bin/rake",
+    path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["alternatives-ruby-$version"]
   }
 
@@ -31,11 +34,13 @@ class ruby($version="1.9.3-p0") {
     command => "gem install bundler",
     unless => "gem list | grep bundler",
     timeout => "-1",
+    path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["alternatives-gem-$version"]
   }
   exec { "alternatives-bundle-$version":
     command => "update-alternatives --install /usr/bin/bundle bundle /opt/ruby-$version/bin/bundle 10 && update-alternatives --set bundle /opt/ruby-$version/bin/bundle",
     unless => "test /etc/alternatives/bundle -ef /opt/ruby-$version/bin/bundle",
+    path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["gem-install-bundler-$version"]
   }
 }
