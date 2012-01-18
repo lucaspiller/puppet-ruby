1fedba9b4cdc2596d08fae38939231a86f095c53
Luca Spiller <luca@stackednotion.com>
2012-01-18 16:58:48 +0000

    Run all update-alternatives commands in quiet mode (warnings cause issues with chaining, but can be ignored)

--- manifests/init.pp
+++ manifests/init.pp
@@ -26,13 +26,13 @@ define rubyinstall {
   }
 
   exec { "alternatives-ruby-$version":
-    command => "update-alternatives --install /usr/bin/ruby ruby /opt/ruby-$version/bin/ruby 10 --slave /usr/bin/irb irb /opt/ruby-$version/bin/irb && update-alternatives --set ruby /opt/ruby-$version/bin/ruby",
+    command => "update-alternatives --quiet --install /usr/bin/ruby ruby /opt/ruby-$version/bin/ruby 10 --slave /usr/bin/irb irb /opt/ruby-$version/bin/irb && update-alternatives --quiet --set ruby /opt/ruby-$version/bin/ruby",
     unless => "test /etc/alternatives/ruby -ef /opt/ruby-$version/bin/ruby && test /etc/alternatives/irb -ef /opt/ruby-$version/bin/irb",
     path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["ruby-install-$version"]
   }
   exec { "alternatives-gem-$version":
-    command => "update-alternatives --install /usr/bin/gem gem /opt/ruby-$version/bin/gem 10 && update-alternatives --set gem /opt/ruby-$version/bin/gem",
+    command => "update-alternatives --quiet --install /usr/bin/gem gem /opt/ruby-$version/bin/gem 10 && update-alternatives --quiet --set gem /opt/ruby-$version/bin/gem",
     unless => "test /etc/alternatives/gem -ef /opt/ruby-$version/bin/gem",
     path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["alternatives-ruby-$version"]
@@ -46,7 +46,7 @@ define rubyinstall {
     require => Exec["alternatives-gem-$version"]
   }
   exec { "alternatives-bundle-$version":
-    command => "update-alternatives --install /usr/bin/bundle bundle /opt/ruby-$version/bin/bundle 10 && update-alternatives --set bundle /opt/ruby-$version/bin/bundle",
+    command => "update-alternatives --quiet --install /usr/bin/bundle bundle /opt/ruby-$version/bin/bundle 10 && update-alternatives --quiet --set bundle /opt/ruby-$version/bin/bundle",
     unless => "test /etc/alternatives/bundle -ef /opt/ruby-$version/bin/bundle",
     path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["gem-install-bundler-$version"]
