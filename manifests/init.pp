3d33369213406a74c9c2f73f55a05e84af9dc2fa
Luca Spiller <luca@stackednotion.com>
2013-03-08 16:32:35 +0000

    Align stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class ruby($version = "1.9.3-p327") {
+class ruby($version = "2.0.0-p0") {
   rubyinstall { "$version": }
 }
 
@@ -40,7 +40,7 @@ define rubyinstall($version = $title) {
       Package["zlib1g"],
       Package["zlib1g-dev"]
     ],
-    path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"]
+    path    => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     timeout => 0
   }
 
@@ -55,35 +55,35 @@ define rubyinstall($version = $title) {
   exec { "ruby-install-$version":
     command => "/opt/ruby-build/bin/ruby-build $version /opt/ruby-$version",
     creates => "/opt/ruby-$version",
-    path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
+    path    => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["ruby-build-update"],
     timeout => 0
   }
 
   exec { "alternatives-ruby-$version":
     command => "update-alternatives --quiet --install /usr/bin/ruby ruby /opt/ruby-$version/bin/ruby 10 --slave /usr/bin/irb irb /opt/ruby-$version/bin/irb && update-alternatives --quiet --set ruby /opt/ruby-$version/bin/ruby",
-    unless => "test /usr/bin/ruby -ef /opt/ruby-$version/bin/ruby && test /usr/bin/irb -ef /opt/ruby-$version/bin/irb",
-    path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
+    unless  => "test /usr/bin/ruby -ef /opt/ruby-$version/bin/ruby && test /usr/bin/irb -ef /opt/ruby-$version/bin/irb",
+    path    => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["ruby-install-$version"]
   }
   exec { "alternatives-gem-$version":
     command => "update-alternatives --quiet --install /usr/bin/gem gem /opt/ruby-$version/bin/gem 10 && update-alternatives --quiet --set gem /opt/ruby-$version/bin/gem",
-    unless => "test /usr/bin/gem -ef /opt/ruby-$version/bin/gem",
-    path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
+    unless  => "test /usr/bin/gem -ef /opt/ruby-$version/bin/gem",
+    path    => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["alternatives-ruby-$version"]
   }
 
   exec { "gem-install-bundler-$version":
     command => "gem install bundler",
-    unless => "gem list | grep bundler",
+    unless  => "gem list | grep bundler",
     timeout => "-1",
-    path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
+    path    => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["alternatives-gem-$version"]
   }
   exec { "alternatives-bundle-$version":
     command => "update-alternatives --quiet --install /usr/bin/bundle bundle /opt/ruby-$version/bin/bundle 10 && update-alternatives --quiet --set bundle /opt/ruby-$version/bin/bundle",
-    unless => "test /usr/bin/bundle -ef /opt/ruby-$version/bin/bundle",
-    path => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
+    unless  => "test /usr/bin/bundle -ef /opt/ruby-$version/bin/bundle",
+    path    => ["/usr/sbin", "/usr/bin", "/sbin", "/bin"],
     require => Exec["gem-install-bundler-$version"]
   }
 }
